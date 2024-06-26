7eba707e4d84913bc15c2b91c645869ef1456344
ryoumura <162957823+ryouMurakami@users.noreply.github.com>
2024-06-26 17:05:55 +0900

    country add

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ app.use("/corona-tracker-world-data", limiter, (req, res, next) => {
   })(req, res, next);
 });
 
+app.use("/corona-tracker-country-data", (req, res, next) => {
+  const city = url.parse(req.url).query;
+  createProxyMiddleware({
+    target: `${process.env.BASE_API_URL_CORONA_COUNTRY}/${city}`,
+    changeOrigin: true,
+    pathRewrite: {
+      [`^/corona-tracker-country-data`]: "",
+    },
+  })(req, res, next);
+});
+
 app.use("/weather-data", limiter, (req, res, next) => {
   const city = url.parse(req.url).query;
   createProxyMiddleware({
