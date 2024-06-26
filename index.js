1bcb6fb38a24a5be9c60d825bdf927f14941c321
ryoumura <162957823+ryouMurakami@users.noreply.github.com>
2024-06-26 16:45:27 +0900

    second commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const rateLimit = require("express-rate-limit");
 require("dotenv").config();
+const url = require("url");
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -23,10 +24,21 @@ app.use("/corona-tracker-world-data", limiter, (req, res, next) => {
   })(req, res, next);
 });
 
-const port = process.env.PORT || 5000
+app.use("/weather-data", limiter, (req, res, next) => {
+  const city = url.parse(req.url).query;
+  createProxyMiddleware({
+    target: `${process.env.BASE_API_URL_WEATHERAPI}${city}$aqi=no`,
+    changeOrigin: true,
+    pathRewrite: {
+      [`^/weather-data`]: "",
+    },
+  })(req, res, next);
+});
+
+const port = process.env.PORT || 5000;
 
 app.listen(5000, () => {
   console.log(`listening on localhost port ${port}`);
 });
 
-module.express = app
+module.express = app;
